8058ad59793aebe60a67bd96a0609443b363a004
jordan-veracitek <49081890+Jordan1022@users.noreply.github.com>
2021-04-29 20:21:51 -0700

    reverted 2 change on root doc

--- swdocdemo/src/assets/RootDoc.js
+++ swdocdemo/src/assets/RootDoc.js
@@ -6,8 +6,8 @@ const text = `
 </style>
 <span class="red">A #|Sweet Will|# is used by an #|iCitizen|# of
 the #|Sweet Economy|#, or simply the #|Economy|#,
-to define their wishes concerning the distribution of their #|owned|# #|Sweet Squares|#,
-or simply #|Squares|#, upon their death.  requires
-each #|iCitizen|# to #|explicitly|# define and declare
+to define their wishes concerning the distribution of their #|owned|# Sweet Squares,
+or simply Squares, upon their death.  requires
+each #|iCitizen|# to explicitly define and declare
 their beneficiaries,</span>`;
 export default text;
